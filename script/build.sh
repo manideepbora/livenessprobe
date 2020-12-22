2abe2d8dc35e02612501a7a1076a07db1a951a04
Manideep Bora <manideepbora@users.noreply.github.com>
2020-12-22 15:01:58 -0500

    Update build.sh
    deleted the account information

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,1 @@
 #!/bin/bash
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 805199394057.dkr.ecr.us-east-2.amazonaws.com
-docker build -t mbora/goserver .
-docker tag mbora/goserver:latest 805199394057.dkr.ecr.us-east-2.amazonaws.com/mbora/goserver:latest
-docker push 805199394057.dkr.ecr.us-east-2.amazonaws.com/mbora/goserver:latest
